53e8943a3cb449c0775a5f42d917302a2bd22b61
Hannah Hall <hannahkhall13@gmail.com>
2017-06-20 11:13:46 -0500

    promise all for coords and countries

--- src/app.jsx
+++ src/app.jsx
@@ -1,58 +1,47 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const axios = require ('axios');
-
-const Country = ({country, guess}) => {
-  // each country
-  return (
-    <div onClick={() => {guess(country.id)}}>{country.name}</div>
-  );
-};
-
-const CountryList = ({countries, guess}) => {
-  const countryNode = countries.map((country) => {
-    return (
-      <Country country={country} key={country.id} guess={guess} />
-    );
-  });
-  return(
-    <div>{countryNode}</div>
-  );
-};
-
-const Header = ({answer, country}) => {
-  return (
-    <div>
-      <button onClick={() => {answer()}}>Where's that country</button>
-      <h1>{country.name}</h1>
-
-    </div>
-  )
-}
+const components = require('./components.jsx')
 
 class MapApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       countries: [],
-      answer: {}
+      answer: {},
+      hint: '',
+      tries: 0
     };
     this.apiUrl = 'https://restcountries.eu/rest/v2/region/Europe';
-
-    this.getCountries = () => {
-      axios.get(this.apiUrl)
-        .then((res) => {
-          res.data.forEach((country, index) => {
-            country.id = index;
-          })
-          console.log("res.data", res.data);
-          this.setState({countries: res.data});
-        })
-    }
-
   }
+
+  getCountries() {
+    axios.all([this.getCountriesProm(), this.getCountryCoords()])
+      .then(axios.spread((countries, coordsData) => {
+        countries.data.forEach((country, index) => {
+          country.id = index;
+          coordsData.data.europe.forEach((coord) => {
+            if (country.name === coord.name) {
+              country.coords = coord.coords
+            }
+          })
+        })
+        console.log("countries.data", countries.data);
+        this.setState({countries: countries.data});
+      })
+    );
+  }
+
   componentDidMount(){
     this.getCountries();
+  }
+
+  getCountriesProm(){
+    return axios.get(this.apiUrl);
+  }
+
+  getCountryCoords(){
+    return axios.get('europe_coords.json');
   }
 
   getAnswer() {
@@ -62,6 +51,7 @@ class MapApp extends React.Component {
   }
 
   guess(id){
+    console.log("id", id);
     if (id === this.state.answer.id) {
       console.log("right answer");
     } else {
@@ -72,11 +62,11 @@ class MapApp extends React.Component {
   render() {
     return (
       <div>
-        <Header
+        <components.Header
           answer={this.getAnswer.bind(this)}
           country={this.state.answer}
         />
-        <CountryList
+        <components.CountryList
           countries={this.state.countries}
           guess={this.guess.bind(this)}
         />
