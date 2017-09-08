d5d724089c860e3efe986342006e9c9efc68fa5e
Hannah Hall <hannahkhall13@gmail.com>
2017-09-08 11:46:34 -0500

    Initial files

---
+++ Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace world_map.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
