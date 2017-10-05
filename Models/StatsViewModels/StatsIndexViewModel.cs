59587341d5bb88b9645ea58fecf86ff9d5819a43
Hannah Hall <hannahkhall13@gmail.com>
2017-10-04 21:29:45 -0500

    new method to group by country then get average

--- Models/StatsViewModels/StatsIndexViewModel.cs
+++ Models/StatsViewModels/StatsIndexViewModel.cs
@@ -7,41 +7,37 @@ namespace WorldMap.Models.StatsViewModels
     public class StatsIndexViewModel
     {
         public IEnumerable<Dictionary<int, int>> OverallStats {get; set;}
-        public IEnumerable<Dictionary<int, int>> EuropeStats {get; set;}
-        public IEnumerable<Dictionary<int, int>> AsiaStats { get; set; }
-        public IEnumerable<Dictionary<int, int>> NorthAmericaStats { get; set; }
-        public IEnumerable<Dictionary<int, int>> SouthAmericaStats { get; set; }
-        public IEnumerable<Dictionary<int, int>> OceaniaStats { get; set; }
-        public IEnumerable<Dictionary<int, int>> AfricaStats { get; set; }
+        public IEnumerable<Dictionary<string, double>> EuropeStats {get; set;}
+        public IEnumerable<Dictionary<string, double>> AsiaStats { get; set; }
+        public IEnumerable<Dictionary<string, double>> NorthAmericaStats { get; set; }
+        public IEnumerable<Dictionary<string, double>> SouthAmericaStats { get; set; }
+        public IEnumerable<Dictionary<string, double>> OceaniaStats { get; set; }
+        public IEnumerable<Dictionary<string, double>> AfricaStats { get; set; }
 
-        public IEnumerable<Dictionary<int, int>> GetTheGroup(List<Stats> stats, string area = null)
+        public IEnumerable<Dictionary<int, int>> GetOverall(List<Stats> stats, string area = null)
         {
-            if (area == null)
-            {
-                return stats.GroupBy(s => s.Success, s => s.StatsId, 
-                    (key, g) => new Dictionary<int, int>(){
-                        {key, g.Count()}
-                    });
-            }
-            else 
-            {
-                return stats.Where(s => s.Country.Continent.Name == area)
-                    .GroupBy(s => s.Success, s => s.StatsId, 
-                        (key, g) => new Dictionary<int, int>() { 
-                            {key, g.Count()} 
-                        });
-            }
+            
+            return stats.GroupBy(s => s.Success, s => s.StatsId, 
+                        (key, g) => new Dictionary<int, int>(){ {key, g.Count()} });
         }
+        public IEnumerable<Dictionary<string, double>> GetAreas(List<Stats> stats, string area)
+        {
+            return stats.OrderBy(s => s.Country.Name).Where(s => s.Country.Continent.Name == area)
+                        .GroupBy(s => s.Country.Name, s => s.Success,
+                            (key, g) => new Dictionary<string, double>() { {key, g.Average()} });
+        }
+
+
 
         public StatsIndexViewModel (List<Stats> stats)
         {
-            OverallStats = GetTheGroup(stats);
-            EuropeStats = GetTheGroup(stats, "Europe");
-            AsiaStats = GetTheGroup(stats, "Asia");
-            NorthAmericaStats = GetTheGroup(stats, "North America");
-            SouthAmericaStats = GetTheGroup(stats, "South America");
-            OceaniaStats = GetTheGroup(stats, "Oceania");
-            AfricaStats = GetTheGroup(stats, "Africa");
+            OverallStats = GetOverall(stats);
+            EuropeStats = GetAreas(stats, "Europe");
+            AsiaStats = GetAreas(stats, "Asia");
+            NorthAmericaStats = GetAreas(stats, "North America");
+            SouthAmericaStats = GetAreas(stats, "South America");
+            OceaniaStats = GetAreas(stats, "Oceania");
+            AfricaStats = GetAreas(stats, "Africa");
         }
     }
 }
