3c704efd1b638932616aa9e6798f206d5658cde3
Hannah Hall <hannahkhall13@gmail.com>
2017-10-04 21:41:07 -0500

    new function to loop through datasets for areas

--- wwwroot/js/statsIndex.js
+++ wwwroot/js/statsIndex.js
@@ -1,31 +1,20 @@
-function MakeChart(dataSet) {
-    if (dataSet.length === 0){
-        $('canvas').hide();
-        $('.para').show();
-        return;
-    } 
-    let data = dataSet.map((s) => {
-        for (let obj in s) {
-            return s[obj]
-        }
-    });
-    $('canvas').show();
-    $('.para').hide();
-    let ctx = $('#percentage')[0].getContext('2d');
+function clearDiv() {
+    $(".canvas-holder").empty();
+    $('.canvas-holder').removeClass('col-md-6 col-md-offset-3');
+}
+
+function createChart (ctx, data) {
     let chart = new Chart(ctx, {
-        // The type of chart we want to create
         type: 'pie',
-        // The data for our dataset
         data: {
             labels: ["Right", "Wrong"],
             datasets: [{
-                label: "My First dataset",
+                label: "Percent Right or Wrong",
                 backgroundColor: ["rgb(255, 99, 132)", "rgb(54, 162, 235)"],
                 borderColor: 'rgb(255, 99, 132)',
                 data: data
             }]
         },
-        // Configuration options go here
         options: {
             legend: {
                 display: true,
@@ -36,12 +25,60 @@ function MakeChart(dataSet) {
         }
     });
 }
+
+function MakeChart(dataSet) {
+    clearDiv();
+    console.log(dataSet);
+    if (dataSet.length === 0){
+        $('.canvas-holder').hide();
+        $('.para').show();
+        return;
+    } 
+    let data = dataSet.map((s) => {
+        for (let obj in s) {
+            return s[obj];
+        }
+    });
+    $('.canvas-holder').show();
+    $('.canvas-holder').addClass('col-md-6 col-md-offset-3');
+    $('.para').hide();
+    $('.canvas-holder').append($('<canvas id="percentage"></canvas>'));
+    let ctx = $('#percentage')[0].getContext('2d');
+    createChart(ctx, data);
+}
 MakeChart(stats.Overall);
+
+function MakeAreaChart (dataset) {
+    console.log(dataset);
+    clearDiv();
+    if (dataset.length === 0) {
+        $('.canvas-holder').hide();
+        $('.para').show();
+        return;
+    }
+    for (item in dataset) {
+        let obj  = dataset[item];
+        var key = Object.keys(obj)[0];
+        let percentageRight = obj[key] * 100;
+        let data = [percentageRight, 100 - percentageRight];
+        let id = key.replace(/\s+/g, "-").replace(/[(,)]/g, '');
+        let $canvasDiv = `<div class=""><h3>${key}</h3><canvas id="${id}"></canvas><div>`;
+        $('.canvas-holder').append($canvasDiv);
+        let ctx = $(`#${id}`)[0].getContext('2d');
+        createChart(ctx, data);
+
+    }
+}
 
 $('a').click((e) => {
     if ($(e.target).attr('for')) {
         $('a').removeClass('active');
         $(e.target).addClass('active');
-        MakeChart(stats[$(e.target).attr('for')]);
+        var which = $(e.target).attr('for');
+        if (which === "Overall") {
+            MakeChart(stats[which]);
+        } else {
+            MakeAreaChart(stats[which]);
+        }       
     }
 });
