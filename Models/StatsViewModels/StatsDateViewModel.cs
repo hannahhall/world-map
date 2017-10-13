0816eb6f71a801ac46dee48a227715cf9ae9e18f
Hannah Hall <hannahkhall13@gmail.com>
2017-10-13 16:03:29 -0500

    groupby within a groupby

--- Models/StatsViewModels/StatsDateViewModel.cs
+++ Models/StatsViewModels/StatsDateViewModel.cs
@@ -7,42 +7,50 @@ namespace WorldMap.Models.StatsViewModels
     public class StatsDateViewModel
     {
         public IEnumerable<Dictionary<DateTime, double>> OverallStats { get; set; }
-        public IEnumerable<Dictionary<DateTime, double>> EuropeStats { get; set; }
-        public IEnumerable<Dictionary<DateTime, double>> AsiaStats { get; set; }
-        public IEnumerable<Dictionary<DateTime, double>> NorthAmericaStats { get; set; }
-        public IEnumerable<Dictionary<DateTime, double>> SouthAmericaStats { get; set; }
-        public IEnumerable<Dictionary<DateTime, double>> OceaniaStats { get; set; }
-        public IEnumerable<Dictionary<DateTime, double>> AfricaStats { get; set; }
+        public IEnumerable<Dictionary<string, IEnumerable<Dictionary<DateTime, double>>>> EuropeStats { get; set; }
+        public IEnumerable<Dictionary<string, IEnumerable<Dictionary<DateTime, double>>>> AsiaStats { get; set; }
+        public IEnumerable<Dictionary<string, IEnumerable<Dictionary<DateTime, double>>>> NorthAmericaStats { get; set; }
+        public IEnumerable<Dictionary<string, IEnumerable<Dictionary<DateTime, double>>>> SouthAmericaStats { get; set; }
+        public IEnumerable<Dictionary<string, IEnumerable<Dictionary<DateTime, double>>>> OceaniaStats { get; set; }
+        public IEnumerable<Dictionary<string, IEnumerable<Dictionary<DateTime, double>>>> AfricaStats { get; set; }
 
-        public IEnumerable<Dictionary<DateTime, double>> GetTheGroup(List<Stats> stats, string area = null)
+        public IEnumerable<Dictionary<DateTime, double>> GetOverall(List<Stats> stats)
         {
-            if (area == null)
-            {
-                return stats.OrderBy(s => s.DateCreated).GroupBy(s => s.DateCreated, s => s.Success,
-                    (key, g) => new Dictionary<DateTime, double>(){
-                        {key, g.Average() * 100}
-                    });
-            }
-            else
-            {
-                return stats.Where(s => s.Country.Continent.Name == area)
-                    .OrderBy(s => s.DateCreated)
-                    .GroupBy(s => s.DateCreated, s => s.Success,
-                        (key, g) => new Dictionary<DateTime, double>(){
-                            {key, g.Average() * 100}
-                    });
-            }
+            
+            return stats.OrderBy(s => s.DateCreated).GroupBy(s => s.DateCreated, s => s.Success,
+                (key, g) => new Dictionary<DateTime, double>(){
+                    {key, g.Average() * 100}
+                });
+            
+        }
+
+        public IEnumerable<Dictionary<string, IEnumerable<Dictionary<DateTime, double>>>> GetAreas (List<Stats> stats, string area)
+        {
+            return stats.Where(s => s.Country.Continent.Name == area)
+                .OrderBy(s => s.Country.Name)
+                .OrderBy(s => s.DateCreated)
+                .GroupBy(s => s.Country.Name, s => s,
+                    (k, g) => new Dictionary<string, IEnumerable<Dictionary<DateTime, double>>>()
+                    {
+                        {k, g.GroupBy(t => t.DateCreated, t => t.Success,
+                            (key, x) => new Dictionary<DateTime, double>()
+                            {
+                                {key, x.Average() * 100}
+                            })
+                        }
+                    }
+                );
         }
 
         public StatsDateViewModel(List<Stats> stats)
         {
-            OverallStats = GetTheGroup(stats);
-            EuropeStats = GetTheGroup(stats, "Europe");
-            AsiaStats = GetTheGroup(stats, "Asia");
-            NorthAmericaStats = GetTheGroup(stats, "North America");
-            SouthAmericaStats = GetTheGroup(stats, "South America");
-            OceaniaStats = GetTheGroup(stats, "Oceania");
-            AfricaStats = GetTheGroup(stats, "Africa");
+            OverallStats = GetOverall(stats);
+            EuropeStats = GetAreas(stats, "Europe");
+            AsiaStats = GetAreas(stats, "Asia");
+            NorthAmericaStats = GetAreas(stats, "North America");
+            SouthAmericaStats = GetAreas(stats, "South America");
+            OceaniaStats = GetAreas(stats, "Oceania");
+            AfricaStats = GetAreas(stats, "Africa");
         }
     
     }
